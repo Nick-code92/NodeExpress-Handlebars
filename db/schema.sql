4513ed2383ea6e242d2dd29b77b995a32893c261
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2020-01-31 22:25:35 -0700

    creating orm.js

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,10 @@ DROP DATABASE IF EXISTS burger_db;
 CREATE DATABASE  burger_db;
 USE burger_db;
 
-CREATE TABLE burgers (
+CREATE TABLE burgers 
+(
 	id INT NOT NULL AUTO_INCREMENT ,
     burger_name VARCHAR(255) NOT NULL,
-    devoured BOOLEAN NOT NULL DEFAULT FALSE,
+    devoured BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (id)
 )
