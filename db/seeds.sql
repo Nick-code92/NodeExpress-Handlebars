4252b6ad5f68e65fe5a521ed569179da8be50817
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2020-01-31 23:05:41 -0700

    modifing burgers controller.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (burger_name) VALUES ('cheese burger');
-INSERT INTO burgers (burger_name) VALUES ('hum burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('vegan burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('smash burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('bacon burger', true);
-INSERT INTO burgers (burger_name) VALUES ('big Mac');
+INSERT INTO burgers (name) VALUES ('cheese burger');
+INSERT INTO burgers (name) VALUES ('hum burger');
+INSERT INTO burgers (name, devoured) VALUES ('vegan burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('smash burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('bacon burger', true);
+INSERT INTO burgers (name) VALUES ('big Mac');
